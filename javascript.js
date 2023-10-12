ab281a3465edad64ce06b2c81b1b9967515c601c
ShadowVectorZ <rayaudet19@gmail.com>
2023-10-12 09:21:39 -0400

    Added game function, point system
    Allows user to play multiple rounds and keep track of points

--- javascript.js
+++ javascript.js
@@ -1,3 +1,6 @@
+let playerScore=0
+let computerScore=0
+
 function getComputerChoice (){
     let randomNumber=Math.floor(Math.random()*3);
     switch (randomNumber){
@@ -14,28 +17,38 @@ function getComputerChoice (){
 }
 
 
+function game() {
+while (playerScore <5 || computerScore<5) {
+    
 
 let playerSelection=prompt("enter Rock, Paper, or Scissors")
-let computerSelection
-computerSelection = getComputerChoice();
-
-
+let computerSelection= getComputerChoice();
+alert(playRound(playerSelection,computerSelection))
+}
+}
 function playRound (playerSelection, computerSelection) {
 
 if (playerSelection.toLowerCase()==="rock"&&computerSelection ==="rock") {
    return declaration="Tie Game!"
+   
 }
 
 if (playerSelection.toLowerCase()==="rock"&&computerSelection==="scissors") {
-   return declaration="You Win!"
+    playerScore+=1;
+    return declaration="You Win!"
+   
 }
 
 if (playerSelection.toLowerCase()==="rock"&&computerSelection==="paper") {
-   return declaration="You Lose!"
+    computerScore+=1
+    return declaration="You Lose!"
+   
 }
 
 if (playerSelection.toLowerCase()==="scissors"&&computerSelection==="paper") {
+    playerScore+=1;
     return declaration="You Win!"
+    
  }
  
  if (playerSelection.toLowerCase()==="scissors"&&computerSelection==="scissors") {
@@ -43,7 +56,9 @@ if (playerSelection.toLowerCase()==="scissors"&&computerSelection==="paper") {
  }
 
  if (playerSelection.toLowerCase()==="scissors"&&computerSelection==="rock") {
+    computerScore+=1
     return declaration="You Lose!"
+   
  }
 
  if (playerSelection.toLowerCase()==="paper"&&computerSelection==="paper") {
@@ -51,13 +66,18 @@ if (playerSelection.toLowerCase()==="scissors"&&computerSelection==="paper") {
  }
 
  if (playerSelection.toLowerCase()==="paper"&&computerSelection==="scissors") {
+    computerScore+=1
     return declaration="You Lose!"
+    
  }
  
  if (playerSelection.toLowerCase()==="paper"&&computerSelection==="rock") {
+    playerScore+=1;
     return declaration="You Win!"
+    
  }
  
+
  
  
  
@@ -69,5 +89,14 @@ if (playerSelection.toLowerCase()==="scissors"&&computerSelection==="paper") {
     console.log(playRound(playerSelection,computerSelection));
 
 }
+function winGame () {
+    if (playerscore=5) {
+        
+        return "You Win!"
 
-    
+        
+    } else if (computerscore=5) {
+     
+        return "You Lose!"
+    }
+}
