c694728aeec5539dc480e985db47a6323e6c65ca
ShadowVectorZ <rayaudet19@gmail.com>
2024-01-16 13:10:50 -0500

    added function that will play single round

--- javascript.js
+++ javascript.js
@@ -1,14 +1,56 @@
 let getComputerChoice = function(){
    let choice = Math.random()*3
    if (choice<=1){
-    choice="Rock"
+    choice="rock"
    }
    else if (choice<=2){
-    choice="Paper"
+    choice="paper"
    }
    else 
-   {choice="Scissors"}
+   {choice="scissors"}
 
    return choice
+
+}
+
+
+let game =function(){
+
+let singleRound = function(){
+   let playerSelection = prompt("Choose your Weapon")
+   if(playerSelection.toLowerCase==="rock"||playerSelection.toLowerCase==="scissors"||playerSelection.toLowerCase==="paper"){
+      
+   }
+   
+   let computerSelection = getComputerChoice()
+
+
+   if (computerSelection===playerSelection.toLowerCase()) {
+      return "Tie Game"
+   
+   }
+
+   else if (computerSelection=="rock"&&playerSelection.toLowerCase()=="paper") {
+      return "Player wins!"
+   }
+
+
+   else if (computerSelection=="paper"&&playerSelection.toLowerCase()=="scissors") {
+      return "Player wins!"
+   }
+
+   else if (computerSelection=="scissors"&&playerSelection.toLowerCase()=="rock") {
+      return "Player wins!"
+   }
+
+   else return "Computer Wins!"
+
+}
+
+console.log(singleRound())
+console.log(singleRound())
+console.log(singleRound())
+console.log(singleRound())
+console.log(singleRound())
 
 }
