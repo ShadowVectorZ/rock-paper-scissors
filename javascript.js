a8f3f52194fb6b8f0617a0462848e36a6b1cb202
ShadowVectorZ <rayaudet19@gmail.com>
2024-01-16 12:05:49 -0500

    added function that gets computer choice

---
+++ javascript.js
@@ -0,0 +1,14 @@
+let getComputerChoice = function(){
+   let choice = Math.random()*3
+   if (choice<=1){
+    choice="Rock"
+   }
+   else if (choice<=2){
+    choice="Paper"
+   }
+   else 
+   {choice="Scissors"}
+
+   return choice
+
+}
