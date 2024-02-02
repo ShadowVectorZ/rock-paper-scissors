e97ee504b33c0ee0460a24848b44d3a36fddadaf
ShadowVectorZ <rayaudet19@gmail.com>
2024-02-02 15:14:00 -0500

    Added code for score and final message

--- javascript.js
+++ javascript.js
@@ -15,65 +15,70 @@ let getComputerChoice = function(){
 
 
 
-     let computerScore=0
+let computerScore=0
 let playerScore=0 
+const score=document.querySelector('#score')
+
 const results =document.querySelector('#results')
 
 let playerSelection
 let singleRound = function(){
 
-    
    let computerSelection = getComputerChoice()
 
 
    if (computerSelection===playerSelection.toLowerCase()) {
+      score.textContent=`Player Score: ${playerScore}. Computer Score: ${computerScore}`
       results.textContent="Tie Game"
    }
    else if (computerSelection=="rock"&&playerSelection.toLowerCase()=="paper") {
       playerScore+=1
+      score.textContent=`Player Score: ${playerScore}. Computer Score: ${computerScore}`
       results.textContent="Player Wins!"
    }
    else if (computerSelection=="paper"&&playerSelection.toLowerCase()=="scissors") {
       playerScore+=1
+      score.textContent=`Player Score: ${playerScore}. Computer Score: ${computerScore}`
       results.textContent="Player Wins!"
    }
    else if (computerSelection=="scissors"&&playerSelection.toLowerCase()=="rock") {
       playerScore+=1
+      score.textContent=`Player Score: ${playerScore}. Computer Score: ${computerScore}`
       results.textContent="Player Wins!"
    }
   else if (computerSelection=="rock"&&playerSelection.toLowerCase()=="scissors") {
       computerScore+=1
+      score.textContent=`Player Score: ${playerScore}. Computer Score: ${computerScore}`
       results.textContent= "Computer wins!"
   }
   else if (computerSelection=="paper"&&playerSelection.toLowerCase()=="rock") {
       computerScore+=1
+      score.textContent=`Player Score: ${playerScore}. Computer Score: ${computerScore}`
       results.textContent= "Computer wins!"
   }
   else if (computerSelection=="scissors"&&playerSelection.toLowerCase()=="paper") {
       computerScore+=1
+      score.textContent=`Player Score: ${playerScore}. Computer Score: ${computerScore}`
       results.textContent= "Computer wins!"
   }
-   else {
-      alert("please choose rock, paper, or scissors")
-      singleRound()
-   }
+ 
+  if (playerScore==5){
+   const container=document.querySelector('#container')
+   container.textContent='The Player Wins The Game! Reload To Play Again!'
+   buttons.remove()
+   
+}
+
+if (computerScore==5){
+   const container=document.querySelector('#container')
+   container.textContent='The Computer Wins The Game! Reload To Play Again!'
+   buttons.remove()
+   
+}
 
 }
-let game =function(){
-/*for (let i=0;i<5;i++){
-   singleRound()
-}
-*/
 
 
-
-
-if (playerScore>computerScore)
-return (`Player has ${playerScore} points! Computer has ${computerScore} points! Player Wins!` )
-
-else return (`Player has ${playerScore} points! Computer has ${computerScore} points! Computer Wins!` )
-
-}
 
 let buttons=document.querySelector('#buttons')
 
