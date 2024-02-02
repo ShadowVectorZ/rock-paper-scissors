c6d249e2f80af60be3867f22a71d40712669f2d8
ShadowVectorZ <rayaudet19@gmail.com>
2024-02-02 13:09:03 -0500

    Moved game function its own thing
    instead of having single round function nestled in it

--- javascript.js
+++ javascript.js
@@ -13,64 +13,55 @@ let getComputerChoice = function(){
 
 }
 
-let playerSelection
 
-let game =function(){
+
      let computerScore=0
 let playerScore=0 
 
 let singleRound = function(){
 
-
+   let playerSelection = prompt("Choose your Weapon")  
    let computerSelection = getComputerChoice()
 
-  
 
-
-   if (computerSelection===playerSelection) {
+   if (computerSelection===playerSelection.toLowerCase()) {
       alert ("Tie Game, play again")
       singleRound()
-   
    }
-
-   else if (computerSelection=="rock"&&playerSelection=="paper") {
+   else if (computerSelection=="rock"&&playerSelection.toLowerCase()=="paper") {
       playerScore+=1
       return "Player wins!"
    }
-
-
-   else if (computerSelection=="paper"&&playerSelection=="scissors") {
+   else if (computerSelection=="paper"&&playerSelection.toLowerCase()=="scissors") {
       playerScore+=1
       return "Player wins!"
    }
-
-   else if (computerSelection=="scissors"&&playerSelection=="rock") {
+   else if (computerSelection=="scissors"&&playerSelection.toLowerCase()=="rock") {
       playerScore+=1
       return "Player wins!"
    }
-
-  else if (computerSelection=="rock"&&playerSelection=="scissors") {
+  else if (computerSelection=="rock"&&playerSelection.toLowerCase()=="scissors") {
       computerScore+=1
       return "Computer wins!"
   }
-
-  else if (computerSelection=="paper"&&playerSelection=="rock") {
+  else if (computerSelection=="paper"&&playerSelection.toLowerCase()=="rock") {
       computerScore+=1
       return "Computer wins!"
   }
-
-
-  else if (computerSelection=="scissors"&&playerSelection=="paper") {
+  else if (computerSelection=="scissors"&&playerSelection.toLowerCase()=="paper") {
       computerScore+=1
       return "Computer wins!"
   }
-  
+   else {
+      alert("please choose rock, paper, or scissors")
+      singleRound()
+   }
 
 }
-
-console.log(singleRound())
-
-
+let game =function(){
+for (let i=0;i<5;i++){
+   singleRound()
+}
 
 if (playerScore>computerScore)
 return (`Player has ${playerScore} points! Computer has ${computerScore} points! Player Wins!` )
@@ -78,24 +69,3 @@ return (`Player has ${playerScore} points! Computer has ${computerScore} points!
 else return (`Player has ${playerScore} points! Computer has ${computerScore} points! Computer Wins!` )
 
 }
-
-
-const rock=document.querySelector('#rock')
-rock.addEventListener('click',function(){
-   playerSelection='rock'
-})
-rock.addEventListener('click',game)
-
-const paper=document.querySelector('#paper')
-paper.addEventListener('click',function(){
-   playerSelection='paper'
-})
-paper.addEventListener('click',game)
-
-const scissors=document.querySelector('#scissors')
-scissors.addEventListener('click',function(){
-   playerSelection='scissors'
-})
-scissors.addEventListener('click',game)
-
-
